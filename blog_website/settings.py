7a84a9c59325c2263bd159045974c87c09669ca6
Jari Oksanen <j.oksanen@icloud.com>
2021-01-20 15:35:11 +0200

    Email hosting settings updated

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -141,3 +141,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'Discussion Boards <noreply@itoksanen.com>'
+EMAIL_SUBJECT_PREFIX = '[Discussion Boards]'
